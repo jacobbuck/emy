8d25b71d724e3c6f11ae8bc79955051736d267d5
Jacob Buck <buck.jacob@gmail.com>
2021-04-16 23:41:31 +0000

    Set `sourcemap: true` on outputs in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -7,10 +7,12 @@ export default {
       exports: 'default',
       file: 'lib/index.cjs.js',
       format: 'cjs',
+      sourcemap: true,
     },
     {
       file: 'lib/index.esm.js',
       format: 'esm',
+      sourcemap: true,
     },
   ],
   plugins: [babel()],
